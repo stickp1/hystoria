60e582a364400ff1351a8b041ed351c6c9b14172
stickp1 <44746442+stickp1@users.noreply.github.com>
2019-07-15 18:01:38 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,110 @@
 const contractSource = `
-  contract MemeVote =
-    record meme =
-      { creatorAddress : address,
-        url            : string,
-        name           : string,
-        voteCount      : int }
-    record state =
-      { memes      : map(int, meme),
-        memesLength : int }
-    function init() =
-      { memes = {},
-        memesLength = 0 }
-    public function getMeme(index : int) : meme =
-      switch(Map.lookup(index, state.memes))
-        None    => abort("There was no meme with this index registered.")
-        Some(x) => x
-    public stateful function registerMeme(url' : string, name' : string) =
-      let meme = { creatorAddress = Call.caller, url = url', name = name', voteCount = 0}
-      let index = getMemesLength() + 1
-      put(state{ memes[index] = meme, memesLength = index })
-    public function getMemesLength() : int =
-      state.memesLength
-    public stateful function voteMeme(index : int) =
-      let meme = getMeme(index)
-      Chain.spend(meme.creatorAddress, Call.value)
-      let updatedVoteCount = meme.voteCount + Call.value
-      let updatedMemes = state.memes{ [index].voteCount = updatedVoteCount }
-      put(state{ memes = updatedMemes })
+  contract History =
+      
+      record now =
+        { witness  : address,
+          moment   : string,
+          name     : string,
+          upVotes  : int,
+          dwVotes  : int }
+          
+      record state =
+        { upPast     : list(now),
+          dwPast     : list(now),
+          timer      : int,
+          nows       : map(int, now),
+          nowsLength : int,
+          major      : int,
+          majorCount : int,
+          minor      : int,
+          minorCount : int,
+          carpeDiem  : int }
+      
+      function init() =
+        { upPast = [],
+          dwPast = [],
+          timer = 0,
+          nows = {},
+          nowsLength = 0,
+          major = 0,
+          majorCount = 0,
+          minor = 0,
+          minorCount = 0,
+          carpeDiem = 0 }
+          
+      public function getNow(index : int) : now =
+        switch(Map.lookup(index, state.nows))
+          None    => abort("There was no moment witnessed by this index.")
+          Some(x) => x
+          
+      public function getNowsLength() : int   = state.nowsLength
+      
+      public function getUpPast() : list(now) = state.upPast
+        
+      public function getDwPast() : list(now) = state.dwPast
+      
+      public function getCarpeDiem() : int = state.carpeDiem
+      
+      public function getMajor() : int = state.major
+      
+      public function getMinor() : int = state.minor
+      
+      public stateful function registerNow(moment' : string, name' : string) =
+        let now = { witness = Call.caller, moment = moment', name = name', upVotes = 0, dwVotes = 0}
+        let index = getNowsLength() + 1
+        //put(state{ nows[index] = now, nowsLength = index, minor = index, minorCount = 0 }) here every new registration becomes the least popular -> write late to win 
+        if ( state.minor == 0 )
+          put(state{ nows[index] = now, nowsLength = index, minor = index, major = index })
+        elif( state.minorCount != 0 )
+          put(state{ nows[index] = now, nowsLength = index, minor = index }) // here the oldest less reputable wins the title -> vote others to win
+        else put(state{ nows[index] = now, nowsLength = index })
+         
+      public stateful function voteUp(index : int) =              // reinforce positive
+        let now = getNow(index)
+        Chain.spend(now.witness, Call.value)
+        let up_upVotes = now.upVotes + Call.value
+        let up_carpeDiem = state.carpeDiem + Call.value
+        let up_Nows = state.nows{ [index].upVotes = up_upVotes }
+        if ( up_upVotes >= state.majorCount )                      // the reacher wins
+          put(state { major = index, majorCount = up_upVotes })
+        put(state{ nows = up_Nows, carpeDiem = up_carpeDiem })
+        isEvent()
+        
+      public stateful function voteDown(index : int) =             // kill negative
+        let now = getNow(index)
+        Chain.spend(now.witness, Call.value)
+        let up_dwVotes = now.dwVotes + Call.value
+        let up_carpeDiem = state.carpeDiem + Call.value
+        let up_Nows = state.nows{ [index].dwVotes = up_dwVotes }
+        if ( index == state.minor ) 
+          let up_minor = findSmallest(1, index, up_Nows)
+          put(state{ minor = up_minor, minorCount = up_Nows[up_minor].dwVotes })
+        put(state{ nows = up_Nows, carpeDiem = up_carpeDiem })
+        isEvent()
+    
+      
+      public function findSmallest(it : int, minor' : int, nows' : map(int, now)) : int = 
+        let candidate = nows'[it]
+        let minor = nows'[minor']
+        let up_minor = minor'
+        if (it < state.nowsLength && candidate.dwVotes < minor.dwVotes) // couldn't do it any other way
+          findSmallest(it + 1, it, nows')
+        elif (it < state.nowsLength)
+          findSmallest(it + 1, minor', nows')
+        elif (candidate.dwVotes < minor.dwVotes)
+          it
+        else 
+          minor'
+      
+      public stateful function isEvent() = 
+        let upNow = state.nows[state.major]
+        let dwNow = state.nows[state.minor]
+        if ( state.carpeDiem > 1000000 )
+          let up_upPast = state.upPast ++ [upNow]
+          let up_dwPast = state.dwPast ++ [dwNow]
+          put(state{ minor = 1})
+          put(state{ upPast = up_upPast, dwPast = up_dwPast, timer @ t = t + 1, nows = {}, nowsLength = 0, major = 0, majorCount = 0, minor = 0, minorCount = 0, carpeDiem = 0})
+        
 `;
 
 //Address of the meme voting smart contract on the testnet of the aeternity blockchain
@@ -85,20 +163,20 @@ window.addEventListener('load', async () => {
 
   //First make a call to get to know how may memes have been created and need to be displayed
   //Assign the value of meme length to the global variable
-  memesLength = await callStatic('getMemesLength', []);
+  memesLength = await callStatic('getNowsLength', []);
 
   //Loop over every meme to get all their relevant information
   for (let i = 1; i <= memesLength; i++) {
 
     //Make the call to the blockchain to get all relevant information on the meme
-    const meme = await callStatic('getMeme', [i]);
+    const meme = await callStatic('getNow', [i]);
 
     //Create meme object with  info from the call and push into the array with all memes
     memeArray.push({
       creatorName: meme.name,
-      memeUrl: meme.url,
+      memeUrl: meme.moment,
       index: i,
-      votes: meme.voteCount,
+      votes: meme.upVotes,
     })
   }
 
@@ -118,7 +196,7 @@ jQuery("#memeBody").on("click", ".voteBtn", async function(event){
       index = event.target.id;
 
   //Promise to execute execute call for the vote meme function with let values
-  await contractCall('voteMeme', [index], value);
+  await contractCall('voteNow', [index], value);
 
   //Hide the loading animation after async calls return a value
   const foundIndex = memeArray.findIndex(meme => meme.index == event.target.id);
@@ -137,7 +215,7 @@ $('#registerBtn').click(async function(){
         url = ($('#regUrl').val());
 
   //Make the contract call to register the meme with the newly passed values
-  await contractCall('registerMeme', [url, name], 0);
+  await contractCall('registerNow', [url, name], 0);
 
   //Add the new created memeobject to our memearray
   memeArray.push({
