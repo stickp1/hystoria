baf9633538116bee37a5feea15b3785372bf4428
stickp1 <44746442+stickp1@users.noreply.github.com>
2019-07-17 23:15:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -85,8 +85,8 @@ const contractSource = `
         isEvent()
     
       
-      public function entrypoint(it : int, minor' : int, nows' : map(int, now)) : int = 
-        let candidate = nows'[it]
+      public entrypoint findSmallest(it : int, minor' : int, nows' : map(int, now)) : int = 
+        let candidate = nws'[it]
         let minor = nows'[minor']
         let up_minor = minor'
         if (it < state.nowsLength && candidate.dwVotes < minor.dwVotes) // couldn't do it any other way
