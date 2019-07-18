aedd6c5f35d11920f6f2c283f66effe738a585a9
stickp1 <44746442+stickp1@users.noreply.github.com>
2019-07-18 22:06:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -254,6 +254,7 @@ jQuery("#nowBody").on("click", ".voteBtn", async function(event){
     console.log(foundIndex);
     nowArray[foundIndex].dwVotes += parseInt(value, 10);
   }
+  console.log("carpeDiem", carpeDiem);
   if(carpeDiem + value > 1000000) {
     pastLength += 1;
     const up = nowArray.sort(function(a,b){return b.upVotes-a.upVotes})
